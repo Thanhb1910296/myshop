1cc211c8d26a4eaad0f1920358183fc1eb396226
Thanhb1910296 <thanhb1910296@student.ctu.edu.vn>
2022-09-11 18:19:24 +0700

    khoi tao du an

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/product_detail_screen.dart';
+import 'package:myshop/ui/products/products_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue, 
+        ).copyWith(secondary: Colors.deepOrange),
+      ),
+      home: SafeArea(
+        child: ProductDetailScreen(
+          ProductManager().items[0],
+        ),
+      ),
+    );
+  }
+}
